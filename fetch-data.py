41da55944d28bfbc34b3a83ce0d7dfad18683eac
Mustafa Turhan <odunluzikkim@gmail.com>
2019-07-19 22:57:27 +0300

    use dict instead of class

--- fetch-data.py
+++ fetch-data.py
@@ -3,8 +3,6 @@ from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from bs4 import BeautifulSoup
-import re
-# import pandas as pd
 import os
 import json
 import io
@@ -23,55 +21,53 @@ driver = webdriver.Firefox(
 driver.implicitly_wait(1)
 driver.get(url)  # go to url
 
-# define class
-class University(object):
-    def __init__(self, name, phone, fax, web, email, address, warden):
-        self.name = name
-        self.phone = phone
-        self.fax = fax
-        self.web = web
-        self.email = email
-        self.address = address
-        self.warden = warden
-
 # define functions
+
+
 def getElementText(afterPrefix):
     return driver.find_element_by_id(unique_id + afterPrefix).text  # get value
 
 
 def appendUniversity():
-    name = getElementText('4g-cap')  # get university name
-    phone = getElementText('bg')  # get university phone
-    fax = getElementText('jg')  # get university fax
-    web = getElementText('rg')  # get university web
-    email = getElementText('zg')  # get university email
-    address = getElementText('6h')  # get university address
-    warden = getElementText('eh')  # get university warden
+    university = dict(
+        name=getElementText('4g-cap'),  # get university name
+        phone=getElementText('bg'),  # get university phone
+        fax=getElementText('jg'),  # get university fax
+        web=getElementText('rg'),  # get university web
+        email=getElementText('zg'),  # get university email
+        address=getElementText('6h'),  # get university address
+        warden=getElementText('eh'),  # get university warden
+    )
 
-    university_list.append(University(
-        name, phone, fax, web, email, address, warden))  # append it to list
+    university_list.append(university)  # append it to list
 
-    close_button = driver.find_element_by_class_name(
-        "z-window-close")  # find close university info window
-    close_button.click()  # click it.
+    try:
+        close_button = driver.find_element_by_class_name(
+            "z-window-close")  # find close university info window
+        close_button.click()  # click it.
+    except:
+        print('Cannot find close button.')
+        driver.quit()
 
 
 def goNextPage():
     try:
-        nextButton = driver.find_element_by_class_name('z-paging-next') # find next button
+        nextButton = driver.find_element_by_class_name(
+            'z-paging-next')  # find next button
+        nextButton.click()  # click it
     except:
         print('Cannot find next button.')
         driver.quit()
 
-    nextButton.click() # click it
-
     try:
-        isNextButtonDisabled = nextButton.get_attribute('disabled') # look for disabled attribute
-        if isNextButtonDisabled == True: # if attribute is true
-            print('Checked every page') # send a message
+        isNextButtonDisabled = nextButton.get_attribute(
+            'disabled')  # look for disabled attribute
+        if isNextButtonDisabled == True:  # if attribute is true
+            print('Checked every page')  # send a message
     except:
-        addUniversitiesToList() # if there is no disabled attribute
-        goNextPage() # go to the next page
+        addUniversitiesToList()  # if there is no disabled attribute
+        goNextPage()  # go to the next page
+
 
 # get unique id's prefix
 try:
@@ -91,22 +87,26 @@ except:
 
 def addUniversitiesToList():
     rowUniversityCount = len(
-        driver.find_elements_by_class_name('z-listcell-content')) # find universities content
+        driver.find_elements_by_class_name('z-listcell-content'))  # find universities content
 
-    for i in range(rowUniversityCount): # for every university in table
-        universityElement = driver.find_elements_by_class_name(
-            "z-listcell-content")[i]  # find university row
-        universityElement.click()  # click to university
-        name = getElementText('4g-cap') # get university name
-        print('Getting:', name.title()) # print university name
-        appendUniversity() # append it.
+    for i in range(rowUniversityCount):  # for every university in table
+        try:
+            universityElement = driver.find_elements_by_class_name(
+                "z-listcell-content")[i]  # find university row
+            universityElement.click()  # click to university
+        except:
+            print('Cannot find university row.')
+            driver.quit()
+        name = getElementText('4g-cap')  # get university name
+        print('Getting:', name.title())  # print university name
+        appendUniversity()  # append it.
 
 
 addUniversitiesToList()
 goNextPage()
 driver.quit()
 
-format_json = [ob.__dict__ for ob in university_list]
+format_json = [ob for ob in university_list]
 with io.open(output_file_name, 'w', encoding='utf8') as outfile:
     json.dump(format_json, outfile, sort_keys=True,
               indent=2, ensure_ascii=False)
